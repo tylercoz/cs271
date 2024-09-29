9213e2accb66bd9680ea760583b17f8a4dd4a123
Tyler Cozzocrea <tylercozzocrea@gmail.com>
2024-09-29 14:10:11 -0700

    Initial commit

---
+++ cploration/c00/main.c
@@ -0,0 +1,13 @@
+/****************************************
+ * C-ploration 0 for CS 271
+ *
+ * [NAME] Tyler Cozzocrea
+ * [TERM] FALL 2024
+ *****************************************/
+#include <stdio.h>
+int main() {
+   // printf(.) displays the string given
+   printf("Hello world!\n");
+   // main returns 0 if exiting without error
+   return 0;
+}
