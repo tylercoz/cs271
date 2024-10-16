95e7f00ee8c77ee052789838769ce0bbf7cf54c0
Tyler Cozzocrea <tylercozzocrea@gmail.com>
2024-10-15 18:57:55 -0700

    Open input file, verify, create out file and open

--- cploration/03/main.c
+++ cploration/03/main.c
@@ -18,5 +18,19 @@ int main(int argc, const char *argv[])
         printf("Usage: %s [filename]\n", argv[0]);
         exit(EXIT_FAILURE);
     }
+
+    FILE *fin = fopen(argv[1], "r");
+
+    if (fin==NULL) {
+        perror("Unable to open file");
+        exit(EXIT_FAILURE);
+    }
+
+    char out_file[MAX_LINE_LENGTH] = "";
+    strcpy(out_file, argv[1]);
+    strcat(out_file, ".echo");
+
+    FILE *fout = fopen(out_file, "w+");
+
 	return 1;
 }
