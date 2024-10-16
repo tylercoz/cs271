ccc67883c3a251e74ad12cc90a41b3f403ccb3db
Tyler Cozzocrea <tylercozzocrea@gmail.com>
2024-10-15 17:14:17 -0700

    Verify one command line argument passed into main

--- cploration/03/main.c
+++ cploration/03/main.c
@@ -6,7 +6,17 @@
  *
  ****************************************/
 
-int main()
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_LINE_LENGTH  200
+
+int main(int argc, const char *argv[])
 {
+    if (argc != 2) {
+        printf("Usage: %s [filename]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
 	return 1;
 }
