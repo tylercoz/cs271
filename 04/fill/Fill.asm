5d5c1188149349ad45ee63962393015fb18ecf49
Tyler Cozzocrea <tylercozzocrea@gmail.com>
2024-11-10 17:47:06 -0800

    implemented fill and mult

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -8,4 +8,98 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+(mainLoop)
+    @KBD
+    D=M
+    @writeBlack
+    D;JGT
+@mainLoop
+0;JMP
+
+(writeWhite)
+    // Initial Setup
+    @SCREEN
+    D=A
+    @counter
+    M=D
+
+    // Set memory to all 0's
+    @counter
+    A=M
+    M=0
+
+    (fillWhite)
+        // Fill white unless key is pressed
+        @KBD
+        D=M
+        @writeBlack
+        D;JGT
+
+        // Increment
+        @counter
+        M=M+1
+
+        // Set memory to all 0's
+        A=M
+        M=0
+
+        // Stop if memory is filled, aka counter is at @24575
+        @counter
+        D=M
+        @24575
+        D=D-A
+    @fillWhite
+    D;JNE
+    
+    @mainLoop
+    0;JMP
+
+
+(writeBlack)
+    // Initial Setup
+    @SCREEN
+    D=A
+    @counter
+    M=D
+
+    // Set memory to all 1's
+    @counter
+    A=M
+    M=-1
+
+    (fillBlack)
+        // Start filling unless key is not pressed
+        @KBD
+        D=M
+        @writeWhite
+        D;JLE
+
+        // Increment
+        @counter
+        M=M+1
+
+        // Set memory to all 1's
+        A=M
+        M=-1
+
+        // Stop if memory is filled, aka counter is at @24575
+        @counter
+        D=M
+        @24575
+        D=D-A
+    @fillBlack
+    D;JNE
+
+    // loop until key is unpressed
+    (waitForKeyUnpressed)
+        @KBD
+        D=M
+        @writeWhite
+        D;JEQ
+    @waitForKeyUnpressed
+    0;JMP
+
+
+
+
+
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -6,5 +6,23 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
+@2
+M=0
+(Loop)
+@1
+D=M
+@Finished
+D;JEQ
+@1
+M=M-1
+@0
+D=M
+@2
+M=D+M
+@Loop
+0;JMP
+(Finished)
 
-//// Replace this comment with your code.
+
+
+
